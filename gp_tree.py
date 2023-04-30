f8d96bd368b46fd0b3c761e7bc4be18feaf2df8f
Blueberry13 <gee8way@gmail.com>
2023-04-30 21:55:29 +0300

    Add deap_fix

--- gp_tree.py
+++ gp_tree.py
@@ -1,21 +1,23 @@
 import numpy as np
-
+import deap_fix
 from tree_runner import run_tree
 from deap import gp
 from gp_terminals.gp_image import GPImage
+
+
+def sigmoid_activation(x):
+    return 1/(1 + np.exp(-x))
+
 
 class GPTree:
     def __init__(self, pset, min_depth: int = 2, max_depth: int = 10, tree: gp.PrimitiveTree = None) -> None:
         self.pset = pset
         if tree is None:
-            expr = gp.genFull(pset, min_=min_depth, max_=max_depth)
+            expr = deap_fix.genFull(pset, min_=min_depth, max_=max_depth)
             self.tree = gp.PrimitiveTree(expr)
         else:
             self.tree = tree
         
     def predict(self, image: GPImage) -> float:
         x = run_tree(image, self.tree)
-        return self.sigmoid_activation(x)
-    
-    def sigmoid_activation(self, x):
-        return 1/(1 + np.exp(-x))
+        return sigmoid_activation(x)

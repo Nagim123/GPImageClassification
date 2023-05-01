175682245f95be5d07af48db0472ecc239f0264c
Nagim123 <fourcolorsgame@gmail.com>
2023-05-01 17:23:56 +0300

    Fix main.py

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from gp_core import GPImageClassifier
 from gp_structures.gp_dataset import GPDataset
-from tools.tree_visualizer import visualize_tree
+from sklearn.metrics import accuracy_score
 
 train_dataset = GPDataset("dataset/train", (20, 20))
 test_dataset = GPDataset("dataset/test", (20, 20))
 gp = GPImageClassifier(population_size=20, generations=15)
 gp.fit(train_dataset)
 
-prediction = gp.predict(test_dataset)
-
-correct = 0
-for i in range(len(prediction)):
-    p = prediction[i]
-    if p > 0.5:
-        p = test_dataset.classes[1]
-    else:
-        p = test_dataset.classes[0]
-    correct += p == test_dataset[i][1]
-print(correct / len(test_dataset))
+print(gp.evaluate(gp.get_best(), test_dataset, ))

52ba84f9a1e180c31929eeae25f21c50c745844d
Nagim123 <fourcolorsgame@gmail.com>
2023-05-01 23:02:41 +0300

    Fix saver

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 def main():
     train_dataset = GPDataset("dataset/train", (20, 20))
     test_dataset = GPDataset("dataset/test", (20, 20))
-    gp = GPImageClassifier(population_size=20, generations=15, n_processes=4)
+    gp = GPImageClassifier(population_size=20, generations=15, n_processes=1)
     gp.fit(train_dataset)
 
     print(gp.evaluate(gp.get_best(), test_dataset, accuracy_score))

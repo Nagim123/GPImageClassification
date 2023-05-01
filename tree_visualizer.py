3404d14ea67cc43a78f51d5d435efc6fd2413e96
Nagim123 <fourcolorsgame@gmail.com>
2023-05-01 13:23:36 +0300

    add tree visualizer

--- tree_visualizer.py
+++ tree_visualizer.py
@@ -1,8 +1,60 @@
 import graphviz
 from deap import gp
+import sys
 
-def visualize_tree(tree: gp.PrimitiveTree):
-    _, edges, labels = gp.graph(tree)
+sys.setrecursionlimit(10000000)
+ind = 0
+
+def tree_parse(content: str, edges, labels):
+    global ind
+    b1 = content.find('(')
+    if b1 == -1:
+        labels[ind] = content
+        ind += 1
+        return ind - 1
+    b2 = content.rfind(')')
+    arg_content = content[b1+1:b2]
+    open_brackets = 0
+    c_data = ""
+    childs = []
+    for i in range(len(arg_content)):
+        if c_data == "ARG0":
+            childs.append(c_data)
+            c_data = ""
+            continue
+        if arg_content[i] == ',' or arg_content[i] == ' ':
+            continue
+        if arg_content[i] == '(':
+            open_brackets += 1
+        elif arg_content[i] == ')':
+            open_brackets -= 1
+            if open_brackets == 0:
+               c_data += arg_content[i]
+               childs.append(c_data)
+               c_data = ""
+               continue
+        c_data += arg_content[i]
+        
+    labels[ind] = content[:b1]
+    c_ind = ind
+    ind += 1
+    if(labels[c_ind] in ["GPFilter", "GPImage", "GPPercentage", "GPPercentageSize", "ARG0"]):
+        return c_ind
+    if(labels[c_ind] == "GPCutshape"):
+        labels[c_ind] = f"GPCutshape({arg_content})"        
+        return c_ind
+    for child in childs:
+        edges.append((c_ind, tree_parse(child, edges, labels)))
+    return c_ind
+
+def visualize_tree(tree: str):
+    global ind
+    
+    edges = []
+    labels = {}
+    ind = 0
+    tree_parse(tree, edges, labels)
+    print(labels)
 
     dot = 'digraph G {\n'
     dot += 'node [shape=circle, style=filled, color=lightblue];\n'
@@ -16,3 +68,5 @@ def visualize_tree(tree: gp.PrimitiveTree):
 
     graph = graphviz.Source(dot)
     graph.render('tree')
+
+visualize_tree(open("best_result_tree.txt", "r").read())
